ce4ee553ba2b29f15842eebdce0ad8ff2ce0cb20
mo-Shu <86178148+mo-Shu@users.noreply.github.com>
2023-07-12 20:14:30 +0900

    first commit

---
+++ Ansbile/wp_setup_pb/roles/wp_03_php_wp/templates/db-config.php
@@ -0,0 +1,18 @@
+<?php
+$wpdb->add_database(array(
+        'host'     => DB_HOST,
+        'user'     => DB_USER,
+        'password' => DB_PASSWORD,
+        'name'     => DB_NAME,
+        'write'    => 1,
+        'read'     => 2,
+));
+
+// $wpdb->add_database(array(
+//         'host'     => "{{ ReadEndpoint_Address }}",
+//         'user'     => DB_USER,
+//         'password' => DB_PASSWORD,
+//         'name'     => DB_NAME,
+//         'write'    => 0,
+//         'read'     => 1,
+// ));
---
+++ README.md
@@ -0,0 +1,6 @@
+# WordPress-on-EC2
+- 2023年7月9日版
+- EC2上ににWordPressを展開してAWSでフルシステム設計してみた
+- CloudFormation + AnsibleでIaC（一部手動）
+
+
